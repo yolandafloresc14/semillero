6a81b88360b788c1fe471317452f901cb6f8bdc1
yolandafloresc14 <yolanda141002@gmail.com>
2024-07-27 17:52:27 -0500

    creacion de component student , nav-bar y header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StudentComponent } from './private/student/student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'student', component: StudentComponent}
+];
---
+++ src/app/private/student/student.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+
+@Component({
+  selector: 'app-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.css'
+})
+export class StudentComponent {
+
+}
