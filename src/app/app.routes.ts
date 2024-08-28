13fec4d02e1594dda4e3d30819a2155a7a49bd7d
AngieUNS <109643676+AngieUNS@users.noreply.github.com>
2024-08-27 21:28:32 -0500

    route-cambio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { StudentComponent } from './private/student/student.component';
 import { AuthenticationComponent } from './public/authentication/authentication.component';
+<<<<<<< Updated upstream
 import { HomeComponent } from './private/student/pages/home/home.component';
 
 export const routes: Routes = [
@@ -10,4 +11,12 @@ export const routes: Routes = [
         ]
     },
     {path: 'login', component: AuthenticationComponent}
+=======
+import { StudentLoginComponent } from './private/student/student-login/student-login.component';
+
+export const routes: Routes = [
+    {path: 'student', component: StudentComponent},
+    {path: 'login', component: AuthenticationComponent},
+    {path: 'studentlogin', component: StudentLoginComponent }
+>>>>>>> Stashed changes
 ];
