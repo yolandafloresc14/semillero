f43a30a20eda4683bd9c6b7d86a931f02bac6c1c
yolandafloresc14 <yolanda141002@gmail.com>
2024-09-24 23:56:36 -0500

    authentication cambios

--- src/app/public/authentication/authentication.component.ts
+++ src/app/public/authentication/authentication.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewEncapsulation, HostBinding } from '@angular/core';
 import { HeaderComponent } from '../../core/components/header/header.component';
 import { Router } from '@angular/router';
 import { StudentLogin1 } from '../../core/components/StudentLogin1/StudentLogin1.component';
+import { AuthService } from "../../core/servicios/roles/auth.service";
+
 @Component({
   selector: 'app-authentication',
   standalone: true,
@@ -9,16 +11,31 @@ import { StudentLogin1 } from '../../core/components/StudentLogin1/StudentLogin1
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.css',
 })
+
 export class AuthenticationComponent {
   @HostBinding('style.display') display = 'contents';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   onStudentButtonClick() {
+<<<<<<< Updated upstream
     this.router.navigate(['/studentlogin']);
   }
 
   onDocenteButtonClick() {
     this.router.navigate(['/docentelogin']);
+=======
+    this.authService.setRole('student')
+    this.router.navigate(["/studentLogin"]);
+    console.log("click desde estudiante")
   }
+
+  onDocenteButtonClick() {
+    this.authService.setRole("teacher")
+    this.router.navigate(["/teacherLogin"]);
+    console.log("click desde docente")
+>>>>>>> Stashed changes
+  }
+
+  
 }
